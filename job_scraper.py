472db5092a516c7dcc090fbf82bdafb75f5b209b
Jordan Williams <Jordan@Emilys-MacBook-Air.local>
2023-04-06 19:42:17 -0400

    Add job_scraper.py with code to make a successful request to Indeed's software engineer job postings for remote work, print a success message if the response status code is 200, and print the HTML content of the response

---
+++ job_scraper.py
@@ -0,0 +1,11 @@
+import requests
+response = requests.get('https://www.indeed.com/jobs?q=software+engineer&l=Remote&sc=0kf%3Aattr%28DSQF7%29explvl%28ENTRY_LEVEL%29%3B&vjk=1435860112a9a13b')
+
+if (response.status_code) == 200:
+    print("successful request")
+else: 
+    print("request failed")
+
+html_content = response.text
+
+print(html_content)
